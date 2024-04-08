0dc7005efa0c8c0f95c679b3dbbb12066b2f4582
Sergi Palouzié <spalouzie@mpm.es>
2024-04-08 21:14:00 +0200

    prueba CD Netlify v3

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Aplicación de notas V2</h1>
+      <h1>Aplicación de notas V3</h1>
       <FormularioDeNotas notas={notas} agregarNota={agregarNota} />
       <ListaDeNotas
         notas={notas}
